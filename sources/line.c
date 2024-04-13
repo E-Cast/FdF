bf146a674aa2ae6f7beae705267b82ceaaf0acc3
E-Cast <emilcastonguay@gmail.com>
2024-04-13 09:37:32 -0400

    draw_line and gradient working, cleaning up now

--- sources/line.c
+++ sources/line.c
@@ -6,29 +6,62 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 03:29:38 by ecastong          #+#    #+#             */
-/*   Updated: 2024/04/13 06:59:01 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/04/13 09:37:17 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	low_slope(mlx_image_t *fdf, t_dot d0, t_dot d1, int yi)
+float	get_fraction(float x1, float x2, float x)
+{
+	if (x1 != x2)
+		return ((x - x1) / (x2 - x1));
+	return (0);
+}
+
+size_t	gradient(t_dot index, t_dot strt, t_dot dest)
+{
+	int		dx;
+	int		dy;
+	float	fraction;
+	int		rgba[4];
+
+	dx = dest.x - strt.sx;
+	dy = dest.y - strt.sy;
+	if (abs(dx) > abs(dy))
+		fraction = get_fraction(strt.sx, dest.sx, index.sx);
+	else
+		fraction = get_fraction(strt.sy, dest.sy, index.sy);
+	rgba[0] = (strt.color >> 24 & 0xFF) + ((dest.color >> 24 & 0xFF)
+			- (strt.color >> 24 & 0xFF)) * fraction;
+	rgba[1] = (strt.color >> 16 & 0xFF) + ((dest.color >> 16 & 0xFF)
+			- (strt.color >> 16 & 0xFF)) * fraction;
+	rgba[2] = (strt.color >> 8 & 0xFF) + ((dest.color >> 8 & 0xFF)
+			- (strt.color >> 8 & 0xFF)) * fraction;
+	rgba[3] = (strt.color & 0xFF) + ((dest.color & 0xFF)
+			- (strt.color & 0xFF)) * fraction;
+	return (rgba[0] << 24 | rgba[1] << 16 | rgba[2] << 8 | rgba[3]);
+}
+
+void	low_slope(mlx_image_t *fdf, t_dot index, t_dot start, t_dot dest)
 {
 	int		dx;
 	int		dy;
 	int		d;
+	int		yi;
 
-	dx = d1.sx - d0.sx;
-	dy = abs(d1.sy - d0.sy);
-	if (d1.sy - d0.sy < 0)
+	dx = dest.sx - start.sx;
+	dy = abs(dest.sy - start.sy);
+	yi = 1;
+	if (dest.sy - start.sy < 0)
 		yi = -1;
 	d = (2 * dy) - dx;
-	while (d0.sx++ < d1.sx)
+	while (index.sx++ < dest.sx)
 	{
-		mlx_put_pixel(fdf, d0.sx, d0.sy, d0.color);
+		mlx_put_pixel(fdf, index.sx, index.sy, gradient(index, start, dest));
 		if (d > 0)
 		{
-			d0.sy = d0.sy + yi;
+			index.sy = index.sy + yi;
 			d = d + (2 * (dy - dx));
 		}
 		else
@@ -36,23 +69,25 @@ void	low_slope(mlx_image_t *fdf, t_dot d0, t_dot d1, int yi)
 	}
 }
 
-void	high_slope(mlx_image_t *fdf, t_dot d0, t_dot d1, int xi)
+void	high_slope(mlx_image_t *fdf, t_dot index, t_dot start, t_dot dest)
 {
 	int	dx;
 	int	dy;
 	int	d;
+	int	xi;
 
-	dx = abs(d1.sx - d0.sx);
-	dy = d1.sy - d0.sy;
-	if (d1.sx - d0.sx < 0)
+	dx = abs(dest.sx - start.sx);
+	dy = dest.sy - start.sy;
+	xi = 1;
+	if (dest.sx - start.sx < 0)
 		xi = -1;
 	d = (2 * dx) - dy;
-	while (d0.sy++ < d1.sy)
+	while (index.sy++ < dest.sy)
 	{
-		mlx_put_pixel(fdf, d0.sx, d0.sy, d0.color);
+		mlx_put_pixel(fdf, index.sx, index.sy, gradient(index, start, dest));
 		if (d > 0)
 		{
-			d0.sx = d0.sx + xi;
+			index.sx = index.sx + xi;
 			d = d + (2 * (dx - dy));
 		}
 		else
@@ -60,22 +95,29 @@ void	high_slope(mlx_image_t *fdf, t_dot d0, t_dot d1, int xi)
 	}
 }
 
-void	draw_line(mlx_image_t *fdf, t_dot *d0, t_dot *d1)
+/**
+ * @brief Draw a line of pixels connecting start and dest.
+ * 
+ * @param fdf Image to draw the pixels on.
+ * @param start Starting dot.
+ * @param dest Destination dot.
+ */
+void	draw_line(mlx_image_t *fdf, t_dot *start, t_dot *dest)
 {
-	mlx_put_pixel(fdf, d0->sx, d0->sy, d0->color);
-	mlx_put_pixel(fdf, d1->sx, d1->sy, d1->color);
-	if (abs(d1->sy - d0->sy) < abs(d1->sx - d0->sx))
+	mlx_put_pixel(fdf, start->sx, start->sy, start->color);
+	mlx_put_pixel(fdf, dest->sx, dest->sy, dest->color);
+	if (abs(dest->sy - start->sy) < abs(dest->sx - start->sx))
 	{
-		if (d0->sx > d1->sx)
-			low_slope(fdf, *d1, *d0, 1);
+		if (start->sx > dest->sx)
+			low_slope(fdf, *dest, *dest, *start);
 		else
-			low_slope(fdf, *d0, *d1, 1);
+			low_slope(fdf, *start, *start, *dest);
 	}
 	else
 	{
-		if (d0->sy > d1->sy)
-			high_slope(fdf, *d1, *d0, 1);
+		if (start->sy > dest->sy)
+			high_slope(fdf, *dest, *dest, *start);
 		else
-			high_slope(fdf, *d0, *d1, 1);
+			high_slope(fdf, *start, *start, *dest);
 	}
 }
--- sources/main.c
+++ sources/main.c
@@ -6,107 +6,11 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 02:58:48 by ecastong          #+#    #+#             */
-/*   Updated: 2024/04/13 03:09:38 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/04/13 09:31:40 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-// static t_data	init_data(int argc, char **argv)
-// {
-// 	t_data	data;
-
-// 	data.xy_scale = XY_SCALE;
-// 	data.z_scale = Z_SCALE;
-// 	data.def_color = DEF_COLOR;
-// 	data.angle = DEF_ANGLE;
-// 	if (argc >= 3)
-// 		data.xy_scale = ft_atoi(argv[2]);
-// 	if (argc >= 4)
-// 		data.z_scale = ft_atoi(argv[3]);
-// 	if (argc == 5)
-// 		data.def_color = my_atoh(argv[4]);
-// 	return (data);
-// }
-
-// void	test_draw_dots(t_data data, t_dot **map, mlx_image_t *img)
-// {
-// 	ssize_t	y_index;
-// 	ssize_t	x_index;
-// 	ssize_t	x_pos;
-// 	ssize_t	y_pos;
-
-// 	x_pos = ((3 + 1) * data.xy_scale - (3 + 1) * data.xy_scale) * cos(data.angle);
-// 	y_pos = ((4 + 1) * data.xy_scale - (3 + 1) * data.xy_scale) * cos(data.angle);
-// 	printf("test1:%ld\n\n", y_pos - x_pos);
-// 	x_pos = ((4 - 3) * data.xy_scale) * cos(data.angle);
-// 	printf("test2:%ld\n\n", x_pos);
-// 	y_index = 0;
-// 	while (y_index < data.max_y)
-// 	{
-// 		x_index = 0;
-// 		while (x_index < data.max_x)
-// 		{
-// 			// x_pos = (x_index * data.xy_scale) * cos(data.angle) + (y_index * data.xy_scale) * cos(data.angle + 2) + (map[y_index][x_index].z * data.z_scale) * cos(data.angle - 2);
-// 			// y_pos = (x_index * data.xy_scale) * sin(data.angle) + (y_index * data.xy_scale) * sin(data.angle + 2) + (map[y_index][x_index].z * data.z_scale) * sin(data.angle - 2);
-// 			// // x_pos = (x_index + 1) * data.xy_scale;
-// 			// // y_pos = (y_index + 1) * data.xy_scale;
-// 			x_pos = ((x_index + 1) * data.xy_scale - (y_index + 1) * data.xy_scale) * cos(data.angle);
-//  			y_pos = ((x_index + 1) * data.xy_scale +  (y_index + 1) * data.xy_scale) * sin(data.angle) - map[y_index][x_index].z * data.z_scale;
-// 			printf("sx:%li/sy%li\nx:%li/y:%li\n\n", x_pos, y_pos, x_index + 1, y_index + 1);
-// 			// printf("x:%i\ny:%i\nx_pos:%i\ny_pos:%i\n\n", x_index + 1, y_index + 1, x_pos + 200, y_pos + 100);
-// 			// if (x_pos > 0 && y_pos > 0)
-// 			mlx_put_pixel(img, x_pos + 170, y_pos + 100, map[y_index][x_index].color);
-// 			x_index++;
-// 		}
-// 		y_index++;
-// 	}
-// }
-
-// void	test(t_data data, t_dot **map)
-// {
-// 	size_t		width;
-// 	size_t		height;
-// 	mlx_t		*mlx;
-// 	mlx_image_t	*img;
-
-// 	width = (data.max_x + data.max_y) * data.xy_scale;
-// 	height = data.max_y * data.xy_scale + data.xy_scale;
-// 	// printf("%li\n%li\n", width, height);
-// 	mlx = mlx_init(width, height + 230, "Test", true);
-// 	if (!mlx)
-// 		return ;
-// 	img = mlx_new_image(mlx, width, width + 230);
-// 	if (!img)
-// 		return ;
-// 	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
-// 		return ;
-// 	test_draw_dots(data, map, img);
-// 	mlx_loop(mlx);
-// 	mlx_delete_image(mlx, img);
-// 	mlx_terminate(mlx);
-// 	(void) map;
-// }
-
-// static void	print_map(t_dot ***map)
-// {
-// 	int		y_index;
-// 	int		x_index;
-
-// 	y_index = 0;
-// 	while (map[y_index])
-// 	{
-// 		x_index = 0;
-// 		while (map[y_index][x_index])
-// 		{
-// 			printf("x:%i/y:%i/z:%i/c:%lx\n",
-// 				map[y_index][x_index]->x, map[y_index][x_index]->y, map[y_index][x_index]->z, map[y_index][x_index]->color);
-// 			x_index++;
-// 		}
-// 		printf("\n");
-// 		y_index++;
-// 	}
-// }
 
 t_mods	init_mods(int argc, char **argv)
 {
