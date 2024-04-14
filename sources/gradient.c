d198661ae277ff325fbbc3e86ccd1cb565870dd3
E-Cast <emilcastonguay@gmail.com>
2024-04-13 21:42:40 -0400

    algo doesn't seem to work, trying something new

--- sources/gradient.c
+++ sources/gradient.c
@@ -6,7 +6,7 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 19:10:00 by ecastong          #+#    #+#             */
-/*   Updated: 2024/04/13 20:01:51 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/04/13 21:39:09 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,12 +80,49 @@ void	normalize_rgba(float *rgba)
 	rgba[3] = rgba[3] / 255.0;
 }
 
-float	inverse_srgb_companding(float n)
+void	inverse_srgb_companding(float *rgba)
 {
-	if (n <= 0.04045)
-		return (n / 12.92);
+	if (rgba[0] <= 0.04045)
+		rgba[0] = (rgba[1] / 12.92);
 	else
-		return (pow((n + 0.055) / 1.055, 2.4));
+		rgba[0] = (pow((rgba[0] + 0.055) / 1.055, 2.4));
+	if (rgba[1] <= 0.04045)
+		rgba[1] = (rgba[1] / 12.92);
+	else
+		rgba[1] = (pow((rgba[1] + 0.055) / 1.055, 2.4));
+	if (rgba[2] <= 0.04045)
+		rgba[2] = (rgba[2] / 12.92);
+	else
+		rgba[2] = (pow((rgba[2] + 0.055) / 1.055, 2.4));
+	if (rgba[3] <= 0.04045)
+		rgba[3] = (rgba[3] / 12.92);
+	else
+		rgba[3] = (pow((rgba[3] + 0.055) / 1.055, 2.4));
+}
+
+float	linear_interpolation(float a, float b, float ratio)
+{
+	return (a * (1.0f - ratio) + b * ratio);
+}
+
+void	interpolate_color(float *strt, float *end, float *rgba, float mix)
+{
+	// rgba[0] = strt[0] * (1.0f - mix) + end[0] * mix;
+	// rgba[1] = strt[1] * (1.0f - mix) + end[1] * mix;
+	// rgba[2] = strt[2] * (1.0f - mix) + end[2] * mix;
+	// rgba[3] = strt[3] * (1.0f - mix) + end[3] * mix;
+	rgba[0] = linear_interpolation(strt[0], end[0], mix);
+	rgba[1] = linear_interpolation(strt[1], end[1], mix);
+	rgba[2] = linear_interpolation(strt[2], end[2], mix);
+	rgba[3] = linear_interpolation(strt[3], end[3], mix);
+}
+
+float	to_sRGB_f(float x)
+{
+	if (x <= 0.0031308)
+		return (12.92 * x);
+	else
+		return ((1.055 * powf(x, 1.0f / 2.4f)) - 0.055);
 }
 
 size_t	gradient(t_dot index, t_dot start, t_dot dest)
@@ -94,53 +131,42 @@ size_t	gradient(t_dot index, t_dot start, t_dot dest)
 	float	dest_color[4];
 	float	ratio;
 	float	color[4];
+	float	gamma;
+	float	brightness1;
+	float	brightness2;
+	float	brightness;
+	float	intensity;
+	float	factor;
 
 	extract_rgba(start.color, start_color);
-	printf("%f/%f/%f/%f\n", start_color[0], start_color[1], start_color[2], start_color[3]);
 	extract_rgba(dest.color, dest_color);
+	printf("%f/%f/%f/%f\n", start_color[0], start_color[1], start_color[2], start_color[3]);
 	ratio = get_ratio(index, start, dest);
 	normalize_rgba(start_color);
-	printf("%f/%f/%f/%f\n\n", start_color[0], start_color[1], start_color[2], start_color[3]);
 	normalize_rgba(dest_color);
-	start_color[0] = inverse_srgb_companding(start_color[0]);
-	start_color[1] = inverse_srgb_companding(start_color[1]);
-	start_color[2] = inverse_srgb_companding(start_color[2]);
-	start_color[3] = inverse_srgb_companding(start_color[3]);
-	printf("%f/%f/%f/%f\n\n", start_color[0], start_color[1], start_color[2], start_color[3]);
-	(void) color;
-	(void) ratio;
-	return (dest.color);
-
-//    //Apply inverse sRGB companding to convert each channel into linear light
-//    r1, g1, b1 ← sRGBInverseCompanding(r1, g1, b1)       
-//    r2, g2, b2 ← sRGBInverseCompanding(r2, g2, b2)
-
-//    //Linearly interpolate r, g, b values using mix (0..1)
-//    r ← LinearInterpolation(r1, r2, mix)
-//    g ← LinearInterpolation(g1, g2, mix)
-//    b ← LinearInterpolation(b1, b2, mix)
-
-//    //Compute a measure of brightness of the two colors using empirically determined gamma
-//    gamma ← 0.43
-//    brightness1 ← Pow(r1+g1+b1, gamma)
-//    brightness2 ← Pow(r2+g2+b2, gamma)
-
-//    //Interpolate a new brightness value, and convert back to linear light
-//    brightness ← LinearInterpolation(brightness1, brightness2, mix)
-//    intensity ← Pow(brightness, 1/gamma)
-
-//    //Apply adjustment factor to each rgb value based
-//    if ((r+g+b) != 0) then
-//       factor ← (intensity / (r+g+b))
-//       r ← r * factor
-//       g ← g * factor
-//       b ← b * factor
-//    end if
-
-//    //Apply sRGB companding to convert from linear to perceptual light
-//    r, g, b ← sRGBCompanding(r, g, b)
-
-//    //Convert color components from 0..1 to 0..255
-//    Result ← MakeColor(r, g, b)
-// End Algorithm MarkMix
+	printf("%f/%f/%f/%f\n", start_color[0], start_color[1], start_color[2], start_color[3]);
+	inverse_srgb_companding(start_color);
+	inverse_srgb_companding(dest_color);
+	printf("%f/%f/%f/%f\n", start_color[0], start_color[1], start_color[2], start_color[3]);
+	interpolate_color(start_color, dest_color, color, ratio);
+	printf("%f/%f/%f/%f\n\n", color[0], color[1], color[2], color[3]);
+	gamma = 0.43;
+	brightness1 = pow(start_color[0] + start_color[1] + start_color[2], gamma);
+	brightness2 = pow(dest_color[0] + dest_color[1] + dest_color[2], gamma);
+	brightness = linear_interpolation(brightness1, brightness2, 1 / gamma);
+	intensity = pow(brightness, 1 / gamma);
+	printf("%f/%f\n\n", brightness, intensity);
+	if ((color[0] + color[1] + color[2]) != 0)
+	{
+		factor = intensity / (color[0] + color[1] + color[2]);
+		color[0] = color[0] * factor;
+		color[1] = color[1] * factor;
+		color[2] = color[2] * factor;
+	}
+	inverse_srgb_companding(color);
+	color[0] = (size_t)(255.9999 * to_sRGB_f(color[0]));
+	color[1] = (size_t)(255.9999 * to_sRGB_f(color[1]));
+	color[2] = (size_t)(255.9999 * to_sRGB_f(color[2]));
+	printf("%lx\n", ((size_t)color[0] << 24 | (size_t)color[1] << 16 | (size_t)color[2] << 8 | 0xFF));
+	return ((size_t)color[0] << 24 | (size_t)color[1] << 16 | (size_t)color[2] << 8 | 0xFF);
 }
